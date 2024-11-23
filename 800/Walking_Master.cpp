bfae324c8e95ac27828ae3c342c8e95682e7fd68
Imdad Miran <miran16@cse.pstu.ac.bd>
2024-11-23 20:38:02 +0600

    Create Walking_Master.cpp

---
+++ 800/Walking_Master.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int t;
+    cin>>t;
+    while(t--){
+        int a,b,c,d;
+		cin>>a>>b>>c>>d;
+		if(b<=d&&c<=a+d-b) {
+			cout<<(d-b)+(a+d-b-c)<<"\n";
+		} else {
+			cout<<"-1\n";
+		}
+	}
+return 0;
+}
