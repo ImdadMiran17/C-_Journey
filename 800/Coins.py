6ca7e5438c918b003a883cae7409512e1bf2902d
Imdad Miran <miran16@cse.pstu.ac.bd>
2024-11-23 19:38:15 +0600

    Create Coins.py

---
+++ 800/Coins.py
@@ -0,0 +1,14 @@
+# import numpy as np
+
+def solve():
+    n,k = map(int, input().split(" "))
+    # arr = list(map(int, input().split(" ")))
+    if n%2 != 0 and k%2 == 0:
+        print("NO")
+    else:
+        print("YES")
+        
+    
+
+for _ in range(int(input())):
+    solve()
